56266f52d3a4d09f7efca2c8a43943597dad5e5d
eric-hole <eric.hole@gmail.com>
2019-11-10 06:25:00 -0800

    COntinued

--- setup.sh
+++ setup.sh
@@ -7,34 +7,27 @@ set -o nounset
 
   
 # Adapted from https://cloud.google.com/blog/products/application-development/build-a-dev-workflow-with-cloud-code-on-a-pixelbook and others
-TMPDIR=$HOME/tmp
-mkdir -p $TMPDIR
-export PATH=/home/erichole/.local/bin:$PATH
-# install pip so we can install configparser
-# #if [ ! $(command -v pip) ]
-# then
-# 	wget https://bootstrap.pypa.io/get-pip.py -O $TMPDIR/gp.pip
-# 	chmod +x $TMPDIR/gp.pip
-# 	python3 $TMPDIR/gp.pip --user
-# fi
-# 
-# pip install --user \
-# 	configparser
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
 
 sudo apt-get update && sudo apt-get install -y \
-    google-cloud-sdk xz-utils \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg2 \
-    software-properties-common \
-    wget \
     gcc g++ \
-    autoconf \
-    libtool \
-    zsh 
+    htop \
+    software-properties-common \
+    xz-utils \
+    zsh
 
-sed -i '1iexport PATH="/usr/lib/google-cloud-sdk/bin:$PATH"' ~/.zshrc
+if [ ! $(command -v go) ]; then
+    mkdir -p "$HOME/go/src" 
+    mkdir -p "$HOME/go/bin"
+    wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+    tar xzf go1.13.4.linux-amd64.tar.gz && sudo mv -n go /usr/local && rm -rf go*
+    export PATH=/usr/local/go/bin:$PATH
+fi
+    go get -u github.com/justjanne/powerline-go
 
 # docker-ce
 if [ ! $(command -v docker) ]
@@ -45,14 +38,16 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y lsb-release docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 fi
 
 # kubectl
-if [ ! $(command -v docker) ]
+if [ ! $(command -v kubectl) ]
 then
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 fi
 
 # vscode
@@ -60,13 +55,14 @@ if [ ! $(command -v code) ]
 then
 TEMP_DEB="$(mktemp).deb" &&
 wget -O "$TEMP_DEB" 'https://go.microsoft.com/fwlink/?LinkID=760868' &&
-sudo apt install "$TEMP_DEB"
+sudo apt install -y "$TEMP_DEB"
 rm -f "$TEMP_DEB"
 fi
 
 # install vscode extensions
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode-remote.remote-containers
+
 
 # set up symlinks
 ln -sf "$PWD/git/gitconfig" "$HOME/.gitconfig"
@@ -88,4 +84,15 @@ ln -sf "$PWD/sql/psqlrc" "$HOME/.psqlrc"
 
 mkdir -p "$HOME/bin"
 export PATH="$HOME/bin:$PATH"
+
+# terminal
+sudo apt install -y gnome-terminal
+
+# fonts
+sudo apt-get install -y fonts-powerline
+mkdir -p "$HOME/.fonts"
+wget https://github.com/tonsky/FiraCode/releases/download/2/FiraCode_2.zip
+unzip FiraCode_2.zip -d FiraCode_2 && mv -n FiraCode_2/ttf/ "$HOME/.fonts"
+rm -rf FiraCode*
+fc-cache -fv
 
